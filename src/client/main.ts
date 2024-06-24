1fd2a2fa89bbf5fb3da9f6ff2b42ab69ee5815a7
Akshit <akshitgautam42@gmail.com>
2024-06-24 22:48:12 +0530

    Final Commit

---
+++ src/client/main.ts
@@ -0,0 +1,59 @@
+import {
+    Keypair,
+    Connection,
+    PublicKey,
+    LAMPORTS_PER_SOL,
+    TransactionInstruction,
+    Transaction,
+    sendAndConfirmTransaction,
+} from '@solana/web3.js';
+
+
+import fs from 'mz/fs';
+import path from 'path';
+
+const My_Program_Keypair = path.join(
+    path.resolve(__dirname, '../../my-solana-keys/contract'),'program-keypair.json'
+);
+async function main() {
+    console.log("Hey, We are launching our Dapp.......");
+
+      // Connecting to solana chain
+      const local_RPC = new Connection('http://localhost:8899', 'confirmed');
+
+      //Get public key
+
+      const secretKeyString = await fs.readFile(My_Program_Keypair, {encoding: 'utf-8'});
+      const secretKey = Uint8Array.from(JSON.parse(secretKeyString));
+      const programKeypair = Keypair.fromSecretKey(secretKey);    
+      let ProgramID: PublicKey = programKeypair.publicKey;
+
+      // Geberating an account keypair to transact with our program
+    const accountKeypair = Keypair.generate();
+    const airdropRequest = await local_RPC.requestAirdrop(
+        accountKeypair.publicKey,
+        LAMPORTS_PER_SOL,
+    );
+    await local_RPC.confirmTransaction(airdropRequest);
+    
+    // Let's Execute Transaction with our program
+    console.log("----------Calling Program ---------------")
+    const Instruction = new TransactionInstruction({
+        keys: [{pubkey: accountKeypair.publicKey, isSigner: false, isWritable:true}],
+        programId: ProgramID,
+        data: Buffer.alloc(0),
+    });
+    await sendAndConfirmTransaction(
+        local_RPC,
+        new Transaction().add(Instruction),
+        [accountKeypair],
+    );
+}
+
+main().then(
+    () => process.exit(),
+    err => {
+        console.error(err);
+        process.exit(-1);
+    },
+);
